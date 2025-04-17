0bc0813284eedb7a7321c98a9a301b8b78907298
mokasp <kasperbouldin@gmail.com>
2025-04-17 04:38:43 -0500

    (add) testing webcam efficiency

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, jsonify
 import tensorflow as tf
+import base64
+from PIL import Image
+from io import BytesIO
+import numpy as np
+
 
 app = Flask(__name__)
 model = tf.keras.models.load_model('test_model_00.keras')
@@ -12,3 +17,15 @@ def index():
 @app.route('/predict')
 def predict():
     return render_template('predict.html')
+
+@app.route('/process_image', methods=['POST'])
+def process_image():
+    data = request.get_json()
+    image_data = data['image']
+
+    encoded = image_data.split(",")[1]
+    decoded = base64.b64decode(encoded)
+    image = Image.open(BytesIO(decoded)).convert('RGB')
+    image_np = np.array(image)
+
+    return jsonify({"status": "ok", "message": "Image recieved and processed"})
