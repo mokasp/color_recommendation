04054b78101afe0ff97dae36785cae81915c65eb
mokasp <kasperbouldin@gmail.com>
2025-04-23 05:13:19 -0500

    (test) comment pout prediction call

--- main.py
+++ main.py
@@ -44,10 +44,10 @@ def process_image():
                 _, buffer = cv2.imencode('.jpg', img)
                 img_base64 = base64.b64encode(buffer).decode('utf-8')
 
-                prediction, input_vectors_lab, output_lab = predict(img, mp_face_mesh, model)
-                logging.debug(f"prediction: {prediction}")
-                logging.debug(f"input_lab_vectors: {input_vectors_lab}")
-                logging.debug(f"output_lab: {output_lab}")
+                # prediction, input_vectors_lab, output_lab = predict(img, mp_face_mesh, model)
+                # logging.debug(f"prediction: {prediction}")
+                # logging.debug(f"input_lab_vectors: {input_vectors_lab}")
+                # logging.debug(f"output_lab: {output_lab}")
 
 
                 return f'<h2>Image received!</h2><img src="data:image/jpeg;base64,{img_base64}" width="1280">'
